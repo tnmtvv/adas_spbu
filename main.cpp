2d420b772597eca3cbbbf4dad42be6a86e809c41
Levliv <livshitz.lewa@yandex.ru>
2022-07-14 22:20:52 +0300

    drawing counturs

--- main.cpp
+++ main.cpp
@@ -43,7 +43,29 @@ int main(int argc, char *argv[])
         cv::warpAffine(frame, rotated_image, rotation_matix, frame.size());
         rotated_image = rotated_image(cv::Rect(0, 0, frame_width, frame_height));
 
-        cv::imshow("frame", rotated_image);
+
+        cv::Mat frame_gray(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+        cv::cvtColor(rotated_image, frame_gray, cv::COLOR_BGR2GRAY);
+
+        int thresh = 10;
+        cv::Mat canny_output(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+        cv::Canny(frame_gray, canny_output, thresh, 200);
+
+        std::vector<std::vector<cv::Point>> contours;
+        std::vector<cv::Vec4i> hierarchy;
+        cv::findContours(canny_output, contours,  hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+        cv::Mat drawing = cv::Mat::zeros(frame.rows, frame.cols, CV_8UC3);
+
+
+        for( size_t i = 0; i< contours.size(); i++ )
+        {
+            //cv::Scalar color = cv::Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256));
+            cv::Scalar color = cv::Scalar(122,  150, 60);
+            drawContours( drawing, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0 );
+        }
+
+        cv::imshow("frame", drawing);
         cv::waitKey(1);
     }
     newFrameCap.release();
