0c2fb294ccfc91166e2ad46be2d749788f2552ff
tnmtvv <62231627+tnmtvv@users.noreply.github.com>
2022-11-25 13:22:18 +0300

    plane mapping added

--- Custom_GA.py
+++ Custom_GA.py
@@ -9,34 +9,57 @@ from sklearn import metrics
 
 class My_GA(GA):
     """Example GA with custom initialize_population method."""
-    def __init__(self, num_of_shots: int, metric: int):
+    def __init__(self, num_of_shots: int, fitness_function: int, num_generation: int, num_population: int):
         GA.__init__(self)
         self.pcds_outliers = []
         self.pcds_inliers = []
+        self.pcds_projected_outliers = []
+        self.generation_goal = num_generation
+        self.population_size = num_population
         self.num_of_shots = num_of_shots
-        self.metric_num = metric
+        self.fitness_funcion_num = fitness_function
 
     def fitting_function(self, chromosome):
-        cloud_number = random.randint(0, len(self.pcds_outliers)-1)
-        points = np.asarray(self.pcds_outliers[cloud_number].points)
+        cloud_number = random.randint(0, self.num_of_shots)
+        points = np.asarray(self.pcds_projected_outliers[cloud_number].points)
         clustering = \
             skc.DBSCAN(eps=chromosome[0].value, min_samples=chromosome[1].value).fit(points)
-
-        silhouette_score = float(metrics.silhouette_score(points, clustering.labels_))
         num_of_clusters = len(np.unique(np.asarray(clustering.labels_)))
 
-        if silhouette_score < 0.1 or num_of_clusters < 7 or num_of_clusters > 150:
-            fitness = 0
-        else:
-            fitness = silhouette_score
-        return fitness
+        if self.fitness_funcion_num == 1:   # silhouette
+            self.target_fitness_type = 'max'
+            silhouette_score = float(metrics.silhouette_score(points, clustering.labels_))
+
+            if silhouette_score < 0.1 or num_of_clusters < 10 or num_of_clusters > 150:
+                fitness = 0
+            else:
+                fitness = silhouette_score
+            return fitness
+        elif self.fitness_funcion_num == 2:  # davies-bouldin
+            self.target_fitness_type = 'min'
+            davies_bouldin_score = float(metrics.davies_bouldin_score(points, clustering.labels_))
+
+            if num_of_clusters < 10 or num_of_clusters > 20:  # need to check
+                fitness = 150
+            else:
+                fitness = davies_bouldin_score
+            return fitness
+        elif self.fitness_funcion_num == 3:  # calinski-harabasz
+            self.target_fitness_type = 'max'
+            calinski_harabasz_score = float(metrics.calinski_harabasz_score(points, clustering.labels_))
+
+            if calinski_harabasz_score < 0.1 or num_of_clusters < 14 or num_of_clusters > 150:
+                fitness = 0
+            else:
+                fitness = calinski_harabasz_score
+            return fitness
 
     def initialize_population(self):
         a_eps = 0.1
         b_eps = 5
 
         a_min_samples = 1
-        b_min_samples = self.population_size
+        b_min_samples = self.population_size/2
 
         h_eps = (b_eps - a_eps)/self.population_size
         h_min_samples = round((b_min_samples - a_min_samples)/self.population_size)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import argparse
 import ast
 import os.path
 import random
+
+from EasyGA.mutation import Mutation
+from AUDI_methods import AUDIMethods
 from os.path import join
 import glob
 import numpy as np
@@ -12,10 +15,9 @@ from sklearn import metrics
 from matplotlib import pyplot as plt
 import EasyGA
 from Custom_GA import My_GA
+import cv2
 
-
-def colours_from_reflectances(reflectances):
-    return np.stack([reflectances, reflectances, reflectances], axis=1)
+from Plane import Plane
 
 
 def run_algo(pcd):
@@ -42,50 +44,50 @@ def run_algo(pcd):
     o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)]+[rest])
 
 
-def create_open3d_pc(lidar, cam_image=None):
-    # create open3d point cloud
-    pcd = o3d.geometry.PointCloud()
+def build_point_clouds_and_lidars(lidar_list):
 
-    # assign point coordinates
-    pcd.points = o3d.utility.Vector3dVector(lidar['points'])
-
-    # assign colours
-    if cam_image is None:
-        median_reflectance = np.median(lidar['reflectance'])
-        colours = colours_from_reflectances(lidar['reflectance']) / (median_reflectance * 5)
-
-    # clip colours for visualisation on a white background
-        colours = np.clip(colours, 0, 0.75)
-    else:
-        rows = (lidar['row'] + 0.5).astype(np.int)
-        cols = (lidar['col'] + 0.5).astype(np.int)
-        colours = cam_image[rows, cols, :] / 255.0
-
-    pcd.colors = o3d.utility.Vector3dVector(colours)
-
-    return pcd
-
-
-def build_point_clouds(root_path):
-    file_names = os.listdir(root_path)
     list_pcds = []
+    list_lidars = []
 
-    for file in file_names:
-        data = np.load(os.path.join(root_path, file))
+    for file in lidar_list:
+        data = np.load(file)
 
         lidar_front_center = {'points': data['pcloud_points'], 'reflectance': data['pcloud_attr.reflectance'],
-                              'timestamp': data['pcloud_attr.timestamp.npy'], 'rows': data['pcloud_attr.row.npy'],
-                              'cols': data['pcloud_attr.col.npy'], 'distance': data['pcloud_attr.distance.npy'],
+                              'timestamp': data['pcloud_attr.timestamp.npy'], 'row': data['pcloud_attr.row.npy'],
+                              'col': data['pcloud_attr.col.npy'], 'distance': data['pcloud_attr.distance.npy'],
                               'depth': data['pcloud_attr.depth.npy'], 'lidar_ids': data['pcloud_attr.lidar_id.npy']}
-        pcd_front_center = create_open3d_pc(lidar_front_center)
+        pcd_front_center = AUDIMethods.create_open3d_pc(lidar_front_center)
         list_pcds.append(pcd_front_center)
+        list_lidars.append(lidar_front_center)
 
-    return list_pcds
+    return list_pcds, list_lidars
 
 
-def ransac_segmentation(list_pcds, distance_threshold=0.3):
+def extract_images(images_path_list):
+    images_list = []
+
+    for file_name in images_path_list:
+        images_list.append(cv2.imread(file_name))
+    return images_list
+
+
+def create_lidar_image_lists(path_to_lidar: str, path_to_images: str):
+    lidar_files = os.listdir(path_to_lidar)
+    images_files = glob.glob(path_to_images + '/*.png')
+
+    lidar_files = sorted(lidar_files, key=lambda x: int(x[-13:-4]))
+    images_files = sorted(images_files, key=lambda x: int(x[-13:-4]))
+
+    list_lidar = list(map(lambda x: os.path.join(path_to_lidar, x), lidar_files))
+    list_images = list(map(lambda x: os.path.join(path_to_images, x), images_files))
+
+    return list_lidar, list_images
+
+
+def ransac_segmentation(list_pcds, distance_threshold=0.4):
     list_outliers = []
     list_inliers = []
+    list_indx = []
 
     for pcd in list_pcds:
         plane_model, inliers = pcd.segment_plane(distance_threshold=distance_threshold, ransac_n=3, num_iterations=1000)
@@ -95,19 +97,42 @@ def ransac_segmentation(list_pcds, distance_threshold=0.3):
 
         list_outliers.append(outlier_cloud)
         list_inliers.append(inlier_cloud)
-    return list_outliers, list_inliers
+        list_indx.append(inliers)
+    return list_outliers, list_inliers, list_indx
 
 
-def main(path_to_data: str, num_shots_to_optimise: int):
+def get_progection(equation, coordinates):
+    # (x - a)/equaion[0] = (y - b)/equation[1] = (z - c)/equation[2]
+    pass
 
-    ga = My_GA(num_shots_to_optimise, 1)
 
-    pcds = build_point_clouds(path_to_data)
-    ga.pcds_outliers, ga.pcds_inliers = ransac_segmentation(pcds, 0.3)
+def main(path_to_lidar: str, path_to_images: str,
+         num_shots_to_optimise: int, generation_goal: int, population_size: int, fitness_function: int, mode: int, verbose=False):
 
+    list_lidar, list_images = create_lidar_image_lists(path_to_lidar, path_to_images)
+
+    pcds, lidars = build_point_clouds_and_lidars(list_lidar)
+    images = extract_images(list_images)
+
+    if num_shots_to_optimise == 0:
+        num_shots_to_optimise = len(list_lidar) - 1
+
+    ga = My_GA(num_shots_to_optimise, fitness_function, generation_goal, population_size)
     ga.chromosome_length = 2
-    ga.population_size = 20
-    ga.generation_goal = 5
+    ga.pcds_outliers, ga.pcds_inliers, list_indx = ransac_segmentation(pcds, 0.125)
+
+    for i, inlier in enumerate(ga.pcds_inliers):
+        plane_equation = Plane.get_equation(inlier.points)
+        print(plane_equation)
+        points_projected = []
+
+        for point in ga.pcds_outliers[i].points:
+            projection = point - (np.dot(plane_equation[:-1], point) + plane_equation[-1]) * plane_equation[:-1]
+            points_projected.append(projection)
+        pcd_projected = o3d.geometry.PointCloud()
+        pcd_projected.points = o3d.utility.Vector3dVector(points_projected)
+
+        ga.pcds_projected_outliers.append(pcd_projected)
 
     ga.chromosome_impl = lambda: [
         random.uniform(0.1, 3),
@@ -115,44 +140,75 @@ def main(path_to_data: str, num_shots_to_optimise: int):
     ]
 
     ga.crossover_population_impl = Crossover.Population.random
-
+    ga.mutation_population_impl = Mutation.Population.random_avoid_best
     ga.fitness_function_impl = ga.fitting_function
-    ga.target_fitness_type = 'max'
 
     while ga.active():
         ga.evolve(1)
-        print('------------------------------')
-        ga.print_population()
-        ga.print_best_chromosome()
-        ga.print_worst_chromosome()
-        print('------------------------------')
-
-    clustering = \
-        skc.DBSCAN(eps=2, min_samples=10).fit(np.asarray(ga.pcds_outliers[0].points))
-    fitness = metrics.silhouette_score(np.asarray(ga.pcds_outliers[0].points), clustering.labels_)
-
-    print("manually: " + str(fitness))
+        if verbose:
+            print('------------------------------')
+            ga.print_population()
+            ga.print_best_chromosome()
+            ga.print_worst_chromosome()
+            print('------------------------------')
 
     best_params = ga.population[0]
-    for pcd_outlier, pcd_inlier in zip(ga.pcds_outliers, ga.pcds_inliers):
+
+    for i, pcd in enumerate(zip(ga.pcds_outliers, ga.pcds_inliers)):
+        pcd_outlier, pcd_inlier = pcd
+
         clustering = skc.DBSCAN(eps=best_params[0].value, min_samples=best_params[1].value).fit(np.asarray(pcd_outlier.points))
         labels = clustering.labels_
         unique_labels = np.unique(labels)
         set_colors = set()
 
-        colors = np.zeros(np.shape(pcd_outlier.points))
+        merged_pcd = pcd_inlier + pcd_outlier
+        colors = np.zeros(np.shape(merged_pcd.points))
+        colors_outlier = np.zeros(np.shape(pcd_outlier.points))
+
         for label in unique_labels:
             if label < 0:
-                colors[np.where(labels == label)] = [0, 0, 0]
+                colors_outlier[np.where(labels == label)] = [0, 0, 0]
             else:
                 cur_color = plt.get_cmap("prism")(random.randint(1, len(unique_labels)))[:3]
-                while cur_color in set_colors or cur_color == [0, 0, 0]:
-                    cur_color = plt.get_cmap("prism")(random.randint(0, label))[:3]
-                colors[np.where(labels == label)] = cur_color
+                colors_outlier[np.where(labels == label)] = cur_color
                 set_colors.add(cur_color)
 
-        pcd_outlier.colors = o3d.utility.Vector3dVector(colors)
-        o3d.visualization.draw_geometries([pcd_outlier, pcd_inlier])
+        pcd_outlier.colors = o3d.utility.Vector3dVector(colors_outlier)
+        merged_pcd_1 = pcd_inlier + pcd_outlier
+
+        if mode == 1:  # only point clouds
+            o3d.visualization.draw_geometries([merged_pcd_1])
+        elif mode == 2: # mapped images
+            mapped_images = []
+
+            cur_image = AUDIMethods.map_lidar_points_onto_image(images[i], lidars[i], colors)
+            mapped_images.append(cur_image)
+
+            plt.fig = plt.figure(figsize=(20, 20))
+            plt.imshow(cur_image)
+            plt.axis('off')
+            plt.close()
+        elif mode == 3:  # with editing
+            cur_inlier_indices = list_indx[i]
+
+            mask = np.ones(colors.shape[0], bool)
+            mask[cur_inlier_indices] = 0
+            colors[mask] = colors_outlier
+
+            vis = o3d.visualization.VisualizerWithEditing()
+            vis.create_window()
+            vis.add_geometry(merged_pcd_1)
+            vis.run()  # user picks points
+            vis.destroy_window()
+            picked_points = vis.get_picked_points()
+
+            mismatched_points = merged_pcd_1.select_by_index(picked_points)
+
+            dists = pcd_inlier.compute_point_cloud_distance(mismatched_points)
+            dists = np.asarray(dists)
+            min_dist = np.min(dists)
+            print (min_dist)
 
 
 if __name__ == '__main__':
@@ -160,15 +216,41 @@ if __name__ == '__main__':
         description="Finds parameters for clusterization algorithms"
     )
     parser.add_argument(
-        "data_path", type=str, help="Directory where main information files are stored"
+        "lidar_path", type=str, help="Directory where main information files are stored"
     )
     parser.add_argument(
+        "images_path", type=str, help="Directory where main information files are stored"
+    )
+
+    parser.add_argument(
         "num_of_shots", type=int, help="Number of shots to optimise"
+    )
+    parser.add_argument(
+        "generation_goal", type=int, help="Number of generations in GA"
+    )
+    parser.add_argument(
+        "population_size", type=int, help="Number of samples in population"
+    )
+    parser.add_argument(
+        "fitness_function", type=int, help="Which fitness functon to use for optimisation: "
+                                           "1 -- silhouette score, 2 -- davies-bouldin score, 3 -- calinski-harabasz"
+    )
+    parser.add_argument(
+        "mode", type=int, help="Choose the mode: 1 -- point clouds, 2 -- mapped images, 3 -- with editing"
+    )
+    parser.add_argument(
+        "verbose", type=bool, help="Print information for each generation"
     )
 
     args = parser.parse_args()
 
     main(
-        args.data_path,
+        args.lidar_path,
+        args.images_path,
         args.num_of_shots,
+        args.generation_goal,
+        args.population_size,
+        args.fitness_function,
+        args.mode,
+        args.verbose
     )
